845babbb315c1572e0536f707f2ddeeeb605aa27
Klinton Lee <klintonlee92@gmail.com>
2021-08-20 02:36:16 -0300

    Upload de arquivos e leitura de dados em chunk com csvParse
    - Implementação do novo `useCase` para importar arquivos de CSV e salva-los;
    - O multer é um middleware que trata arquivos e enviar para `request.file`;
    - O `fs` é nativo do node, que permitiu realizar a leitura deste arquivo.csv em
      partes (chunk);
    - A lib de `csv-parse` está recebendo o conteúdo em `chunk` e tratando o `CSV`,
      como detectar quebras de linha e o delimitador (vírgula por default);
    - Para aguardar o processo assíncrono finalizar, foi aplicado o `new Promise()`;

---
+++ src/modules/cars/useCases/importCategories/ImportCategoriesUseCase.ts
@@ -0,0 +1,50 @@
+/* eslint-disable no-undef */
+import fs from 'fs';
+import csvParse from 'csv-parse';
+import { ICategoriesRepository } from '../../repositories/ICategoriesRepository';
+
+interface IImportCategory {
+  name: string;
+  description: string;
+}
+
+class ImportCategoriesUseCase {
+  constructor(private categoriesRepository: ICategoriesRepository) {}
+
+  private async loadCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
+    const categories: Array<IImportCategory> = [];
+
+    return new Promise((resolve, reject) => {
+      const stream = fs.createReadStream(file.path);
+
+      const parseFile = csvParse();
+
+      stream.pipe(parseFile);
+
+      parseFile
+        .on('data', line => {
+          const [name, description] = line;
+
+          categories.push({ name, description });
+        })
+        .on('end', () => {
+          fs.promises.unlink(file.path);
+          resolve(categories);
+        })
+        .on('error', error => reject(error));
+    });
+  }
+
+  async execute(file: Express.Multer.File): Promise<void> {
+    const categories = await this.loadCategories(file);
+
+    categories.forEach(category => {
+      const categoryExists = this.categoriesRepository.findByName(category.name);
+      if (!categoryExists) {
+        this.categoriesRepository.create(category);
+      }
+    });
+  }
+}
+
+export { ImportCategoriesUseCase };
