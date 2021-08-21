55a1bc7cbca2a22c373dad349a876221dd7055f7
Klinton Lee <klintonlee92@gmail.com>
2021-08-21 19:28:33 -0300

    Documentação da API com Swagger
    - Implementação da biblioteca `swagger-ui-express` e disponibilização dele na
      rota '/swagger';
    - Criação do arquivo swagger.json que será montado pela lib;

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import express from 'express';
+import swaggerUiExpress from 'swagger-ui-express';
 import { routes } from './routes';
+import swaggerDoc from './swagger.json';
 
 const app = express();
 
 app.use(express.json());
 app.use('/v1', routes);
+app.use('/swagger', swaggerUiExpress.serve, swaggerUiExpress.setup(swaggerDoc));
 
 app.listen(3333, () => 'Servidor rodando');
